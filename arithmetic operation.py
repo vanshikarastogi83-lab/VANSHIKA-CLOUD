d47c773aadfad1175a05d0b51b8ed48d71484d12
VANSHIKA RASTOGI <vanshikarastogi83@gmail.com>
2026-03-24 20:16:05 +0530

    Add basic arithmetic operations in Python

---
+++ arithmetic operation.py
@@ -0,0 +1,10 @@
+a = 10
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
